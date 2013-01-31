579ba6f9aa538bd0bbb38fcd91e064da827a4dfb
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-31 15:30:33 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCatarse::Application.config.secret_token = '0a0157a0baf2eaf2db5451e5a4f5f3631752d920b3c75ac61a948d351b729b1f1237aa778029d5a3dd5f1201d751de90e127e957238ef32f529c54c38fa354c4'
